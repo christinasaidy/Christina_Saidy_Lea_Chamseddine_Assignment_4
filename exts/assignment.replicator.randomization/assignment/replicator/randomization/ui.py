ac327ca02b5e2313a0849171abc414df34906e5e
christinasaidy <christinasaidy04@gmail.com>
2025-07-15 18:03:11 +0300

    feat: add writer UI

--- exts/assignment.replicator.randomization/assignment/replicator/randomization/ui.py
+++ exts/assignment.replicator.randomization/assignment/replicator/randomization/ui.py
@@ -19,6 +19,9 @@ class SpawnControlsUI:
                 #camera configuration
                 self.camera_configuration()
 
+                #writer configuration
+                self.writer_configuration()
+
                 #Spawn Button
                 self.button_spawn()
 
@@ -119,7 +122,7 @@ class SpawnControlsUI:
                 with ui.HStack(spacing=6, height=28):
                     ui.Label("Light Type", style={"font_size": 12, "min_width": 30})
                     self.light_options =["distant", "Sphere", "Dome"]
-                    self.combo_lights = ui.ComboBox(0, *self.light_options, width =80)
+                    self.combo_lights = ui.ComboBox(0, *self.light_options, width =100)
                     #reference for combobox : "https://github.com/mati-nvidia/developer-office-hours/blob/main/exts/maticodes.doh_2022_09_23/scripts/combobox_selected_item.py"
 
     def camera_configuration(self): 
@@ -129,25 +132,48 @@ class SpawnControlsUI:
                         ui.Label("Number of Cameras:", style={"font_size": 12, "min_width": 50})
                         self.cam_numb= ui.IntField(width=50, height=20)
                     with ui.HStack(spacing =6, height = 28):
-                        ui.Label("Scatter Mode:", style={"font_size": 12, "min_width": 90})
+                        ui.Label("Scatter on Plane:", style={"font_size": 12, "min_width": 90})
                         self.scatter_checkbox = ui.CheckBox()
-                        ui.Label("Plane To Scatter On:", style={"font_size": 12, "min_width": 90})
-                        self.plane_scatter = ui.StringField()
+                        # ui.Label("Plane To Scatter On:", style={"font_size": 12, "min_width": 90})
+                        # self.plane_scatter = ui.StringField(height =20)
+
+    def writer_configuration(self):
+        with ui.CollapsableFrame("Writer and Output Controls", opened=True):
+            with ui.VStack(spacing=4, padding=5):
+                with ui.HStack(spacing=6, height=28):
+                    ui.Label("Writer Type:", style={"font_size": 12, "min_width": 90})
+                    self.writer_options = ["BasicWriter", "KittiWriter", "WorkerWriter"]
+                    self.combobox_writers = ui.ComboBox(0, *self.writer_options, width=100)
+
+                with ui.HStack(spacing=6, height=28):
+                    ui.Label("Output Directory:", style={"font_size": 12, "min_width": 90})
+                    self.output_dir = ui.StringField(width=200, height= 20)
+
+                with ui.HStack(spacing=6, height=28):
+                    ui.Label("Number of Frames:", style={"font_size": 12, "min_width": 90})
+                    self.num_frames = ui.IntField(width=50, height = 20)
+
+                ui.Label("Output Types:", style={"font_size": 12})
+                with ui.HStack(spacing=6, padding=6, height =22):
+                    ui.Label("RGB", style={"font_size": 12, "min_width": 90})
+                    self.rgb_checkbox = ui.CheckBox()
+                    ui.Label("2D Bounding Box", style={"font_size": 12, "min_width": 90})
+                    self.boundbox_checkbox = ui.CheckBox()
+                    # ui.Label("Point Cloud", style={"font_size": 12, "min_width": 90})
+                    # self.pointcloud_checkbox = ui.CheckBox()
 
     def button_spawn(self):
         with ui.HStack(height=30, padding=10):
             self.button = ui.Button("Spawn", clicked_fn=self.build_scene)
-
-
 
     def build_scene(self):
         object_settings = self.get_object_settings()
         transform_settings = self.get_transform_settings()
         lighting_settings = self.get_lighting_settings()
         camera_settings = self.get_camera_settings()
+        writer_settings = self.get_writer_settings()
 
-        object_spawn(
-            object_settings["spawn_cubes"], object_settings["spawn_spheres"], object_settings["spawn_cylinders"],
+        object_spawn(object_settings["spawn_cubes"], object_settings["spawn_spheres"], object_settings["spawn_cylinders"],
             object_settings["cube_count"], object_settings["sphere_count"], object_settings["cylinder_count"],
             object_settings["spawn_colors"], 
             transform_settings["position_min"], transform_settings["position_max"],
@@ -158,7 +184,9 @@ class SpawnControlsUI:
             lighting_settings["light_type"],
             object_settings["has_material"],
             camera_settings["camera_number"], camera_settings["scatter_mode"],
-            camera_settings["scatter_plane"])
+            # camera_settings["scatter_plane"],
+            writer_settings["writer_type"], writer_settings["output_directory"], writer_settings["frame_number"],
+            writer_settings["output_rgb"], writer_settings["output_bounding_box"])
         
     def get_object_settings(self):
         return {
@@ -174,33 +202,26 @@ class SpawnControlsUI:
 
     def get_transform_settings(self):
         return {
-            "position_min": (
-                self.pos_min_x.model.get_value_as_float(),
+            "position_min": (self.pos_min_x.model.get_value_as_float(),
                 self.pos_min_y.model.get_value_as_float(),
-                self.pos_min_z.model.get_value_as_float()
-            ),
-            "position_max": (
-                self.pos_max_x.model.get_value_as_float(),
+                self.pos_min_z.model.get_value_as_float()),
+            "position_max": (self.pos_max_x.model.get_value_as_float(),
                 self.pos_max_y.model.get_value_as_float(),
-                self.pos_max_z.model.get_value_as_float()
-            ),
-            "rotation_min": (
-                self.rot_min_x.model.get_value_as_float(),
+                self.pos_max_z.model.get_value_as_float()),
+
+            "rotation_min": (self.rot_min_x.model.get_value_as_float(),
                 self.rot_min_y.model.get_value_as_float(),
-                self.rot_min_z.model.get_value_as_float()
-            ),
-            "rotation_max": (
-                self.rot_max_x.model.get_value_as_float(),
+                self.rot_min_z.model.get_value_as_float()),
+
+            "rotation_max": (self.rot_max_x.model.get_value_as_float(),
                 self.rot_max_y.model.get_value_as_float(),
-                self.rot_max_z.model.get_value_as_float()
-            ),
-            "scale_min": (
-                self.scal_min_x.model.get_value_as_float(),
+                self.rot_max_z.model.get_value_as_float()),
+
+            "scale_min": (self.scal_min_x.model.get_value_as_float(),
                 self.scal_min_y.model.get_value_as_float(),
-                self.scal_min_z.model.get_value_as_float()
-            ),
-            "scale_max": (
-                self.scal_max_x.model.get_value_as_float(),
+                self.scal_min_z.model.get_value_as_float()),
+                
+            "scale_max": (self.scal_max_x.model.get_value_as_float(),
                 self.scal_max_y.model.get_value_as_float(),
                 self.scal_max_z.model.get_value_as_float())}
 
@@ -217,6 +238,16 @@ class SpawnControlsUI:
     def get_camera_settings(self):
         return {
             "camera_number": self.cam_numb.model.get_value_as_int(),
-            "scatter_mode": self.scatter_checkbox.model.get_value_as_bool(),
-            "scatter_plane": self.plane_scatter.model.get_value_as_string()}
+            "scatter_mode": self.scatter_checkbox.model.get_value_as_bool()}
+            # "scatter_plane": self.plane_scatter.model.get_value_as_string()}
+    
+    def get_writer_settings(self):
+        selected_index = self.combobox_writers.model.get_item_value_model().as_int
+        writer_type = self.writer_options[selected_index]
+        return{
+            "writer_type": writer_type,
+            "output_directory": self.output_dir.model.get_value_as_string(),
+            "frame_number": self.num_frames.model.get_value_as_int(),
+            "output_rgb": self.rgb_checkbox.model.get_value_as_bool(),
+            "output_bounding_box": self.boundbox_checkbox.model.get_value_as_bool()}
 
